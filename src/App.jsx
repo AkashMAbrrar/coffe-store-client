bc6de46d538faede1807b9f5a405f0c46814c12e
AkashMAbrrar <madborakash48@gmail.com>
2024-11-28 12:28:44 +0600

    this is the first commit for adding all the files and neccessary packages installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-orange-950 font-bold text-center">
+        Coffee Store
+      </h1>
+    </>
+  );
+}
+
+export default App;
